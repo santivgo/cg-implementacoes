0ffec0b26871941f89905c730a7980097b3d6605
santivgo <requiemsantiago@gmail.com>
2025-04-01 11:31:44 -0300

    first commit

---
+++ color_shifting/app.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import sys
+
+import cairo
+import gi
+from Pylette import extract_colors, Color
+import numpy as np
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+import colorsys
+
+
+
+def get_palette(filename):
+    palette = extract_colors(image=filename, palette_size=8)
+    for p in palette:       
+        print(p.hsv)
+    # rgb_palette = [palette.rgb for p in palette]
+
+    return palette
+
+def shifting(palette):
+    total_shifting = 0
+    """
+    A ideia aqui seria fazer o seguinte, a cada iteração o total shifting aumentaria 1 até chegar 
+    ao tamanho total da paleta, cada paleta procuraria o índice da sua cor na lista, e ao encontrar,
+    procuraria a cor indiceDaCor + total_shifting (if > lista.length indice_novo % tamanho_da_paleta )
+    """
+
+    array = np.array(surface.get_data())
+    data = np.frombuffer(surface.get_data(), dtype=np.uint32) 
+    data = data.reshape((height, width))
+    r = (data >> 16) & 0xFF  # Pegando os bits do vermelho
+    g = (data >> 8) & 0xFF   # Pegando os bits do verde
+    b = data & 0xFF          # Pegando os bits do azul
+    rgb_array = np.stack([r, g, b], axis=-1) 
+
+    for y in range(height):
+        for x in range(width):
+            r, g, b = rgb_array[y, x]  # Pegando os valores RGB
+            print(f"Pixel ({x},{y}): R={r}, G={g}, B={b}")
+            #print(hex(data[y, x]))
+            # if (hex(data[y, x]) in palette):
+def draw_event(widget, ctx, surface):
+    ctx.set_source_surface(surface, 0, 0)
+    ctx.paint()
+
+filename = 'waterfall.png'
+surface = cairo.ImageSurface.create_from_png(filename)
+width = surface.get_width()
+height = surface.get_height()
+palette = get_palette(filename)
+
+shifting(palette)
+
+win = Gtk.Window()
+win.connect('destroy', Gtk.main_quit)
+
+drawingarea = Gtk.DrawingArea()
+win.add(drawingarea)
+drawingarea.connect('draw', draw_event, surface)
+drawingarea.set_size_request(width, height)
+
+win.show_all()
+Gtk.main()
