b9870c2a1bc427a892ccf7608b3ef39ffbf2918a
santivgo <requiemsantiago@gmail.com>
2025-04-22 11:14:27 -0300

    pedras entrando no buraco

--- sokoban/js/sokoban.js
+++ sokoban/js/sokoban.js
@@ -1,22 +1,19 @@
-import TileMap  from "./tilemap.js";
-
+import TileMap from "./tilemap.js";
 
 const tileSize = 32;
 
-const cnv = document.querySelector("#sokobanCanvas")
+const cnv = document.querySelector("#sokobanCanvas");
 const ctx = cnv.getContext("2d");
 const tileMap = new TileMap(tileSize);
-const player = tileMap.getPlayer()
-const stone = tileMap.getStones()
+const player = tileMap.getPlayer();
+const stones = tileMap.getStones();
 
 setInterval(gameLoop, 1000 / 60);
 
-
 function gameLoop() {
-    tileMap.draw(cnv, ctx);
-    player.draw(ctx)
-    stone.forEach(stone => {
-        stone.draw(ctx)
-    });
+  tileMap.draw(cnv, ctx);
+  player.draw(ctx);
+  tileMap.stones.forEach((stone) => {
+    stone.draw(ctx);
+  });
 }
-
--- sokoban/js/tilemap.js
+++ sokoban/js/tilemap.js
@@ -3,28 +3,24 @@ import Stone from "./stone.js";
 import MovingDirection from "./moviment.js";
 
 export default class TileMap {
+  constructor(tileSize) {
+    this.tileSize = tileSize;
 
-    constructor(tileSize) {
-        this.tileSize = tileSize;
+    this.floor = this.#image("floor.png");
+    this.wall = this.#image("wall.png");
+    this.stone = this.#image("stone.png");
+    this.player = this.#image("player.png");
+    this.hole = this.#image("hole.png");
+    this.stones = [];
+  }
 
-        this.floor = this.#image('floor.png')
-        this.wall = this.#image('wall.png')
-        this.stone = this.#image('stone.png')
-        this.player = this.#image('player.png')
-        this.hole = this.#image('hole.png')
+  // 1 - parede
+  // 0 - caminho
+  // 2 - boneco
+  // 3 - blocos arrastaveis
+  // 4 - buracos
 
-
-    }
-
-
-    // 1 - parede
-    // 0 - caminho
-    // 2 - boneco
-    // 3 - blocos arrastaveis
-    // 4 - buracos
-
-
-map = [
+  map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 3, 0, 1, 0, 4, 0, 1, 0, 3, 0, 1, 0, 4, 0, 1],
@@ -38,145 +34,200 @@ map = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-];
+  ];
 
+  #image(file) {
+    const img = new Image();
+    img.src = `../img/${file}`;
+    return img;
+  }
+  #setCanvasSize(canvas) {
+    canvas.height = this.map.length * this.tileSize;
+    canvas.width = this.map[0].length * this.tileSize;
+  }
+  #clearCanvas(canvas, ctx) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
 
-    #image(file) {
-        const img = new Image();
-        img.src = `../img/${file}`
-        return img
-    }
-    #setCanvasSize(canvas){
-        canvas.height = this.map.length * this.tileSize
-        canvas.width = this.map[0].length * this.tileSize
-    }
-    #clearCanvas(canvas, ctx){
-        ctx.fillStyle = "black"
-        ctx.fillRect(0,0,canvas.width, canvas.height)
+  #drawMap(ctx) {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        const tile = this.map[row][column];
+        let image = null;
 
-    }
-
-    #drawMap(ctx){
-        for(let row = 0; row < this.map.length; row++){
-            for(let column = 0; column < this.map[row].length; column++){
-                const tile = this.map[row][column]
-                let image = null;
-
-                switch(tile){
-                    case 1:
-                        image = this.wall;
-                        break
-                    case 0:
-                        image = this.floor;
-                        break
-                    case 2:
-                        image = this.player;
-                        break
-                    case 3:
-                        image = this.stone;
-                        break
-                    case 4: 
-                        image = null;
-                        break
-                }
-
-                ctx.strokeStyle = "yellow"
-                ctx.strokeRect(
-                    column* this.tileSize,
-                    row*this.tileSize,
-                    this.tileSize, this.tileSize
-                )
-
-                if(image != null){
-                    ctx.drawImage(image, column * this.tileSize,
-                row * this.tileSize,
-                this.tileSize, this.tileSize)
-                }
-           
-            }
-        }
-    }
-
-
-
-
-
-    getPlayer(){
-        for(let row = 0; row < this.map.length; row++){
-            for(let column = 0; column < this.map[row].length; column++){
-                const tile = this.map[row][column];
-                if(tile === 2){
-                    this.map[row][column] = 0
-                    return new Player(
-                        column*this.tileSize,
-                        row*this.tileSize,
-                        this.tileSize, this
-                    )
-                }
-            }
-        }
-    }
-
-    getStones(){
-        const stones = []
-        for(let row = 0; row < this.map.length; row++){
-            for(let column = 0; column < this.map[row].length; column++){
-                const tile = this.map[row][column];
-                if(tile === 3){
-                    this.map[row][column] = 0
-                    stones.push(new Stone(column*this.tileSize, row*this.tileSize, this.tileSize, this))
-                }
-            }
+        switch (tile) {
+          case 1:
+            image = this.wall;
+            break;
+          case 0:
+            image = this.floor;
+            break;
+          case 2:
+            image = this.player;
+            break;
+          case 3:
+            image = this.stone;
+            break;
+          case 4:
+            image = this.hole;
+            break;
         }
 
-        return stones
+        ctx.strokeStyle = "yellow";
+        ctx.strokeRect(
+          column * this.tileSize,
+          row * this.tileSize,
+          this.tileSize,
+          this.tileSize
+        );
+
+        if (image != null) {
+          ctx.drawImage(
+            image,
+            column * this.tileSize,
+            row * this.tileSize,
+            this.tileSize,
+            this.tileSize
+          );
+        }
+      }
     }
+  }
 
-    draw(canvas, ctx) {
-        this.#setCanvasSize(canvas)
-        this.#clearCanvas(canvas, ctx)
-        this.#drawMap(ctx)
+  getPlayer() {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        const tile = this.map[row][column];
+        if (tile === 2) {
+          this.map[row][column] = 0;
+          return new Player(
+            column * this.tileSize,
+            row * this.tileSize,
+            this.tileSize,
+            this
+          );
+        }
+      }
     }
+  }
 
-    collide(x, y, moviment){
-        if(Number.isInteger(x/this.tileSize) && Number.isInteger(y/this.tileSize)){
-            let column = 0
-            let row = 0
-            let nextColumn = 0
-            let nextRow = 0
+  getStones() {
+    const stones = [];
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        const tile = this.map[row][column];
+        if (tile === 3) {
+          this.map[row][column] = 0;
+          stones.push(
+            new Stone(
+              column * this.tileSize,
+              row * this.tileSize,
+              this.tileSize,
+              this
+            )
+          );
+        }
+      }
+    }
+    this.stones = stones;
+    return stones;
+  }
 
-            switch(MovingDirection[moviment]){
-                case 'up':
-                    nextRow = y - this.tileSize;
-                    row = nextRow / this.tileSize;
-                    column = x / this.tileSize;
-                    break
-                case 'down':
-                    nextRow = y + this.tileSize;
-                    row = nextRow / this.tileSize;
-                    column = x / this.tileSize;
-                    break
-                case 'left':
-                    nextColumn = x - this.tileSize;
-                    column = nextColumn / this.tileSize;    
-                    row = y / this.tileSize;
-                    break
-                case 'right':
-                    nextColumn = x + this.tileSize;
-                    column = nextColumn / this.tileSize;    
-                    row = y / this.tileSize;
-                    break
-            }
-            const tile = this.map[row][column];
-            if(tile === 1){
-                return true
-            }else if (tile === 3){
+  draw(canvas, ctx) {
+    this.#setCanvasSize(canvas);
+    this.#clearCanvas(canvas, ctx);
+    this.#drawMap(ctx);
+  }
 
-                console.log(this.map[nextRow][nextColumn])
-                return true
-            }
+  collide(x, y, moviment) {
+    if (
+      Number.isInteger(x / this.tileSize) &&
+      Number.isInteger(y / this.tileSize)
+    ) {
+      let column = 0;
+      let row = 0;
+      let nextColumn = 0;
+      let nextRow = 0;
+
+      switch (MovingDirection[moviment]) {
+        case "up":
+          nextRow = y - this.tileSize;
+          row = nextRow / this.tileSize;
+          column = x / this.tileSize;
+          break;
+        case "down":
+          nextRow = y + this.tileSize;
+          row = nextRow / this.tileSize;
+          column = x / this.tileSize;
+          break;
+        case "left":
+          nextColumn = x - this.tileSize;
+          column = nextColumn / this.tileSize;
+          row = y / this.tileSize;
+          break;
+        case "right":
+          nextColumn = x + this.tileSize;
+          column = nextColumn / this.tileSize;
+          row = y / this.tileSize;
+          break;
+      }
+
+      const tile = this.map[row][column];
+
+      if (tile === 1) {
+        return true;
+      }
+
+      const hasStone = this.stones.find(
+        (stone) =>
+          stone.x === column * this.tileSize && stone.y === row * this.tileSize
+      );
+
+      if (hasStone) {
+        let behindColumn = column;
+        let behindRow = row;
+
+        switch (MovingDirection[moviment]) {
+          case "up":
+            behindRow--;
+            break;
+          case "down":
+            behindRow++;
+            break;
+          case "left":
+            behindColumn--;
+            break;
+          case "right":
+            behindColumn++;
+            break;
         }
 
-        return false
+        const tileBehind = this.map[behindRow][behindColumn];
+        const stoneBehind = this.stones.find(
+          (stone) =>
+            stone.x === behindColumn * this.tileSize &&
+            stone.y === behindRow * this.tileSize
+        );
+
+        const canMoveStone =
+          (tileBehind === 0 || tileBehind === 4) && !stoneBehind;
+
+        if (canMoveStone) {
+          if (tileBehind === 4)
+            this.stones = this.stones.filter((stone) => stone !== hasStone);
+          else {
+            hasStone.x = behindColumn * this.tileSize;
+            hasStone.y = behindRow * this.tileSize;
+          }
+
+          return false;
+        } else {
+          return true;
+        }
+      }
     }
+
+    return false;
+  }
 }
