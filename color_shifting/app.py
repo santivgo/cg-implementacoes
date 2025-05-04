fcb7abd859ec73fa3295c0490d94eb15eb7eeb7a
santivgo <requiemsantiago@gmail.com>
2025-05-04 17:48:29 -0300

    finalizando o color shifting

--- color_shifting/app.py
+++ color_shifting/app.py
@@ -6,34 +6,36 @@ import gi
 import numpy as np
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk,GLib
-import colorsys
 
+def convert_hex(hex):
+    return hex.replace("#", "0xff")
 
 def getAllColors(color):
     file = open('colors.txt', 'a')
     file.write(f"{color}\n")
     file.close()
-def convert_hex(hex):
-    return hex.replace("#", "0xff")
 
-def shifting(palette, surface):
+
+
+def shifting():
     """
     A ideia aqui seria fazer o seguinte, a cada iteração o total shifting aumentaria 1 até chegar 
     ao tamanho total da paleta, cada paleta procuraria o índice da sua cor na lista, e ao encontrar,
     procuraria a cor indiceDaCor + total_shifting (if > lista.length indice_novo % tamanho_da_paleta )
     """
 
-    data = np.frombuffer(surface.get_data(), dtype=np.uint32) 
-    data = data.reshape((height, width))
+    arr = np.frombuffer(surface.get_data(), dtype=np.uint32).reshape((height, width))
     
     # tentar transformar isso em um set para nao se verificar todas as cores
 
-    for i in range(len(palette)):
-        for y in range(height):
-            for x in range(width):
-                getAllColors(hex(data[y,x]))
-                data[y,x] = get_next_color(hex(data[y,x]), palette) # problema: paleta de cores não mt legal
-        surface.write_to_png(f"output{i}.png")
+    for y in range(height):
+        for x in range(width):
+            h = hex(arr[y,x])
+            arr[y,x] = get_next_color(h, palette) # problema: paleta de cores não mt legal
+    surface.mark_dirty()
+    drawingarea.queue_draw()
+    return True
+
 
     # print("atualizou 1 cor")
     # surface.mark_dirty()
@@ -70,25 +72,20 @@ og_surface = cairo.ImageSurface.create_from_png(filename)
 width = og_surface.get_width()
 height = og_surface.get_height()
 
-# surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height) # duvida, usar esse formato msm?
-
-shifting(palette, og_surface)
-ctx = cairo.Context(og_surface)
+surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height) # duvida, usar esse formato msm?
+ctx = cairo.Context(surface)
 ctx.set_source_surface(og_surface, 0, 0)
 ctx.paint() 
+
 
 
 win = Gtk.Window()
 win.connect('destroy', Gtk.main_quit)
 drawingarea = Gtk.DrawingArea()
-
-
-
-win.add(drawingarea)
-drawingarea.connect('draw', draw_event, og_surface)
+drawingarea.connect('draw', draw_event, surface)
 drawingarea.set_size_request(width, height)
-
+win.add(drawingarea)
 win.show_all()
-# GLib.timeout_add(100, shifting)
+GLib.timeout_add(200, shifting)
 # 
 Gtk.main()
